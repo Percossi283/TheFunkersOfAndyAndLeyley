6ceb807e51309700a3c29863f5d4ea5ab842e07b
Percossi283 <eros.percossi@gmail.com>
2024-10-09 07:51:43 +0200

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -74,16 +74,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['YOU PIECE SOME SHIT >:(', 0.2], //From 0% to 19%
+		['wha', 0.4], //From 20% to 39%
+		['YOU LOSER', 0.5], //From 40% to 49%
+		['Uh', 0.6], //From 50% to 59%
+		['Uh Wow', 0.69], //From 60% to 68%
+		['Nice buddy', 0.7], //69%
+		['Great', 0.8], //From 70% to 79%
+		['Wow!', 0.9], //From 80% to 89%
+		['SUPER!', 1], //From 90% to 99%
+		['SUPER COMBO!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
@@ -2486,7 +2486,7 @@ class PlayState extends MusicBeatState
 	public var totalPlayed:Int = 0;
 	public var totalNotesHit:Float = 0.0;
 
-	public var showCombo:Bool = false;
+	public var showCombo:Bool = true;
 	public var showComboNum:Bool = true;
 	public var showRating:Bool = true;
 
